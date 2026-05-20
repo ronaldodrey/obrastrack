d37d0ecb610bb54a4338ab64b86e05f2fc2e7b39
ronaldodrey <eng.ronaldodrey@gmail.com>
2026-05-20 20:32:20 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@
 //  Você obterá esses dados no passo 3 do guia de instalação.
 // ══════════════════════════════════════════════════════
 const firebaseConfig = {
-  apiKey:            "COLE_AQUI_apiKey",
-  authDomain:        "COLE_AQUI_authDomain",
-  projectId:         "COLE_AQUI_projectId",
-  storageBucket:     "COLE_AQUI_storageBucket",
-  messagingSenderId: "COLE_AQUI_messagingSenderId",
-  appId:             "COLE_AQUI_appId"
+  apiKey:            "AIzaSyCkNV2xCFAOAZm8aoFuiZrLOXcMyh84iDI",
+  authDomain:        "obrastrack-a4fcc.firebaseapp.com",
+  projectId:         "obrastrack-a4fcc",
+  storageBucket:     "obrastrack-a4fcc.firebasestorage.app",
+  messagingSenderId: "602986862632",
+  appId:             "1:602986862632:web:dababf9b41d2e17319bd1d"
 };
 
 export default firebaseConfig;
