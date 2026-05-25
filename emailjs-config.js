aa9d2346b8baa934c0e66c9f2e48c096d193f424
ronaldodrey <eng.ronaldodrey@gmail.com>
2026-05-25 20:25:11 -0300

    Create emailjs-config.js

---
+++ emailjs-config.js
@@ -0,0 +1,14 @@
+const EMAILJS_CONFIG = {
+  publicKey:          "COLE_AQUI_publicKey",
+  serviceId:          "COLE_AQUI_serviceId",
+  tplObraConcluida:   "COLE_AQUI_template_obra_concluida",
+  tplPrazoPerto:      "COLE_AQUI_template_prazo_perto",
+  tplPrazoCritico:    "COLE_AQUI_template_prazo_critico",
+  tplPendencia:       "COLE_AQUI_template_pendencia",
+  tplMedidaPrazo:     "COLE_AQUI_template_medida_prazo",
+  emailGerente:       "COLE_AQUI_email_gerente@empresa.com",
+  diasAvisoObra:      15,
+  diasAvisoMedida:    5,
+  diasCritico:        1,
+};
+export default EMAILJS_CONFIG;
